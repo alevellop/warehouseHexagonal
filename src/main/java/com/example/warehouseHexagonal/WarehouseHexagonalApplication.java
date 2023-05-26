c7a248c30e6b990840b0df47d374b3f68fb2bc88
Alejandro <alevellop@gmail.com>
2023-05-26 09:17:07 +0200

    Initial commit

---
+++ src/main/java/com/example/warehouseHexagonal/WarehouseHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.example.warehouseHexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseHexagonalApplication.class, args);
+	}
+
+}
